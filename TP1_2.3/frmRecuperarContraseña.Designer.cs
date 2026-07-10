53eda691392ce9af54ea1f993d7ed6cead4f4b6e
Malena Moralez <malenanoemimoralez3@gmail.com>
2026-07-10 23:44:29 +0200

    Cree el formulario de Recuperar Contraseña, pero nose porque no compila, lo vamos a ver con mi amiga Cami

---
+++ TP1_2.3/frmRecuperarContraseña.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace TP1_2._3
+{
+    partial class frmRecuperarContraseña
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblNombreRecu = new Label();
+            lblNombreusuario = new Label();
+            txtNombreusuario = new TextBox();
+            txtContraseñanueva = new TextBox();
+            lblContraseña1 = new Label();
+            btnEnviarcodigo = new Button();
+            label3 = new Label();
+            lblmensaje = new Label();
+            btnConfirmar = new Button();
+            btnVolver = new Button();
+            lblConfirmarcontraseña = new Label();
+            txtConfirmarcontraseña = new TextBox();
+            lblCodigoenviado = new Label();
+            txtCodigoenviado = new TextBox();
+            SuspendLayout();
+            // 
+            // lblNombreRecu
+            // 
+            lblNombreRecu.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lblNombreRecu.AutoSize = true;
+            lblNombreRecu.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblNombreRecu.ForeColor = Color.DarkBlue;
+            lblNombreRecu.Location = new Point(65, 9);
+            lblNombreRecu.Name = "lblNombreRecu";
+            lblNombreRecu.Size = new Size(299, 37);
+            lblNombreRecu.TabIndex = 0;
+            lblNombreRecu.Text = "Recuperar Contraseña";
+            // 
+            // lblNombreusuario
+            // 
+            lblNombreusuario.AutoSize = true;
+            lblNombreusuario.ForeColor = Color.DarkBlue;
+            lblNombreusuario.Location = new Point(12, 62);
+            lblNombreusuario.Name = "lblNombreusuario";
+            lblNombreusuario.Size = new Size(110, 15);
+            lblNombreusuario.TabIndex = 1;
+            lblNombreusuario.Text = "Nombre de Usuario";
+            // 
+            // txtNombreusuario
+            // 
+            txtNombreusuario.Location = new Point(12, 80);
+            txtNombreusuario.Name = "txtNombreusuario";
+            txtNombreusuario.Size = new Size(215, 23);
+            txtNombreusuario.TabIndex = 2;
+            // 
+            // txtContraseñanueva
+            // 
+            txtContraseñanueva.Enabled = false;
+            txtContraseñanueva.Location = new Point(12, 240);
+            txtContraseñanueva.Name = "txtContraseñanueva";
+            txtContraseñanueva.PasswordChar = '*';
+            txtContraseñanueva.Size = new Size(215, 23);
+            txtContraseñanueva.TabIndex = 4;
+            // 
+            // lblContraseña1
+            // 
+            lblContraseña1.AutoSize = true;
+            lblContraseña1.ForeColor = Color.DarkBlue;
+            lblContraseña1.Location = new Point(12, 222);
+            lblContraseña1.Name = "lblContraseña1";
+            lblContraseña1.Size = new Size(102, 15);
+            lblContraseña1.TabIndex = 3;
+            lblContraseña1.Text = "Nueva contraseña";
+            // 
+            // btnEnviarcodigo
+            // 
+            btnEnviarcodigo.BackColor = Color.RoyalBlue;
+            btnEnviarcodigo.ForeColor = Color.Black;
+            btnEnviarcodigo.Location = new Point(12, 109);
+            btnEnviarcodigo.Name = "btnEnviarcodigo";
+            btnEnviarcodigo.Size = new Size(215, 28);
+            btnEnviarcodigo.TabIndex = 5;
+            btnEnviarcodigo.Text = "Enviar Codigo";
+            btnEnviarcodigo.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.DarkBlue;
+            label3.Location = new Point(247, 130);
+            label3.Name = "label3";
+            label3.Size = new Size(197, 21);
+            label3.TabIndex = 7;
+            label3.Text = "¿Olvidate tu contraseña?";
+            // 
+            // lblmensaje
+            // 
+            lblmensaje.AutoSize = true;
+            lblmensaje.Location = new Point(274, 151);
+            lblmensaje.Name = "lblmensaje";
+            lblmensaje.Size = new Size(153, 75);
+            lblmensaje.TabIndex = 8;
+            lblmensaje.Text = "No te preocupes, podemos \r\nayudarte a reestablecerla. \r\nIngresa el usuario con el \r\ncual iniciaste sesion \r\nanteriormente.";
+            // 
+            // btnConfirmar
+            // 
+            btnConfirmar.Cursor = Cursors.Hand;
+            btnConfirmar.Location = new Point(12, 329);
+            btnConfirmar.Name = "btnConfirmar";
+            btnConfirmar.Size = new Size(215, 26);
+            btnConfirmar.TabIndex = 10;
+            btnConfirmar.Text = "Guardar";
+            btnConfirmar.UseVisualStyleBackColor = true;
+            btnConfirmar.Click += btnConfirmar_Click;
+            // 
+            // btnVolver
+            // 
+            btnVolver.Location = new Point(247, 330);
+            btnVolver.Name = "btnVolver";
+            btnVolver.Size = new Size(180, 26);
+            btnVolver.TabIndex = 11;
+            btnVolver.Text = "Volver al Inicio de sesion";
+            btnVolver.UseVisualStyleBackColor = true;
+            // 
+            // lblConfirmarcontraseña
+            // 
+            lblConfirmarcontraseña.AutoSize = true;
+            lblConfirmarcontraseña.ForeColor = Color.DarkBlue;
+            lblConfirmarcontraseña.Location = new Point(12, 266);
+            lblConfirmarcontraseña.Name = "lblConfirmarcontraseña";
+            lblConfirmarcontraseña.Size = new Size(122, 15);
+            lblConfirmarcontraseña.TabIndex = 12;
+            lblConfirmarcontraseña.Text = "Confirmar contraseña";
+            // 
+            // txtConfirmarcontraseña
+            // 
+            txtConfirmarcontraseña.Enabled = false;
+            txtConfirmarcontraseña.Location = new Point(12, 284);
+            txtConfirmarcontraseña.Name = "txtConfirmarcontraseña";
+            txtConfirmarcontraseña.PasswordChar = '*';
+            txtConfirmarcontraseña.Size = new Size(215, 23);
+            txtConfirmarcontraseña.TabIndex = 13;
+            // 
+            // lblCodigoenviado
+            // 
+            lblCodigoenviado.AutoSize = true;
+            lblCodigoenviado.ForeColor = Color.DarkBlue;
+            lblCodigoenviado.Location = new Point(65, 161);
+            lblCodigoenviado.Name = "lblCodigoenviado";
+            lblCodigoenviado.Size = new Size(91, 15);
+            lblCodigoenviado.TabIndex = 14;
+            lblCodigoenviado.Text = "Codigo Enviado";
+            // 
+            // txtCodigoenviado
+            // 
+            txtCodigoenviado.Enabled = false;
+            txtCodigoenviado.Location = new Point(12, 179);
+            txtCodigoenviado.Name = "txtCodigoenviado";
+            txtCodigoenviado.PlaceholderText = "Ingrese el codigo recibido";
+            txtCodigoenviado.Size = new Size(215, 23);
+            txtCodigoenviado.TabIndex = 15;
+            txtCodigoenviado.TextAlign = HorizontalAlignment.Center;
+            // 
+            // frmRecuperarContraseña
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(456, 368);
+            Controls.Add(txtCodigoenviado);
+            Controls.Add(lblCodigoenviado);
+            Controls.Add(txtConfirmarcontraseña);
+            Controls.Add(lblConfirmarcontraseña);
+            Controls.Add(btnVolver);
+            Controls.Add(btnConfirmar);
+            Controls.Add(lblmensaje);
+            Controls.Add(label3);
+            Controls.Add(btnEnviarcodigo);
+            Controls.Add(txtContraseñanueva);
+            Controls.Add(lblContraseña1);
+            Controls.Add(txtNombreusuario);
+            Controls.Add(lblNombreusuario);
+            Controls.Add(lblNombreRecu);
+            Name = "frmRecuperarContraseña";
+            Text = "frmRecuperarContraseña";
+            Load += frmRecuperarContraseña_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNombreRecu;
+        private Label lblNombreusuario;
+        private TextBox txtNombreusuario;
+        private TextBox txtContraseñanueva;
+        private Label lblContraseña1;
+        private Button btnEnviarcodigo;
+        private Label label3;
+        private Label lblmensaje;
+        private Button btnConfirmar;
+        private Button btnVolver;
+        private Label lblConfirmarcontraseña;
+        private TextBox txtConfirmarcontraseña;
+        private Label lblCodigoenviado;
+        private TextBox txtCodigoenviado;
+    }
+}
---
+++ TP1_2.3/frmRecuperarContraseña.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TP1_2._3
+{
+    public partial class frmRecuperarContraseña : Form
+    {
+        public frmRecuperarContraseña()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRecuperarContraseña_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNombreusuario.Text)) 
+            {
+                MessageBox.Show("Para Guardar debe Ingresar el Nombre de usuario registrado");
+                txtNombreusuario.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(txtCodigoenviado.Text))
+            {
+                MessageBox.Show("Para Guardar debe Ingresar el Codigo que recibio");
+                txtCodigoenviado.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(txtContraseñanueva.Text))
+            {
+                MessageBox.Show("Para Guardar ingrese su nueva contraseña");
+                txtContraseñanueva.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(txtConfirmarcontraseña.Text))
+            {
+                MessageBox.Show("Para Guardar ingrese la confirmacion de su contraseña");
+                txtConfirmarcontraseña.Focus();
+                return;
+            }
+
+        }
+    }
+}
