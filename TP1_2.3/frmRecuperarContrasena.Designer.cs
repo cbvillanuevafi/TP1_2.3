d30058db0d5084cf125c7bde137384884d3c65e2
Maximiliano Montiel <maxy.montiel@gmail.com>
2026-07-11 17:39:53 -0300

    Cambio el text de frmRecuperarContrasena

--- TP1_2.3/frmRecuperarContrasena.Designer.cs
+++ TP1_2.3/frmRecuperarContrasena.Designer.cs
@@ -50,9 +50,9 @@
             lblNombreRecu.AutoSize = true;
             lblNombreRecu.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblNombreRecu.ForeColor = Color.DarkBlue;
-            lblNombreRecu.Location = new Point(74, 12);
+            lblNombreRecu.Location = new Point(65, 9);
             lblNombreRecu.Name = "lblNombreRecu";
-            lblNombreRecu.Size = new Size(370, 46);
+            lblNombreRecu.Size = new Size(299, 37);
             lblNombreRecu.TabIndex = 0;
             lblNombreRecu.Text = "Recuperar Contraseña";
             // 
@@ -60,37 +60,35 @@
             // 
             lblNombreusuario.AutoSize = true;
             lblNombreusuario.ForeColor = Color.DarkBlue;
-            lblNombreusuario.Location = new Point(14, 83);
+            lblNombreusuario.Location = new Point(12, 62);
             lblNombreusuario.Name = "lblNombreusuario";
-            lblNombreusuario.Size = new Size(139, 20);
+            lblNombreusuario.Size = new Size(110, 15);
             lblNombreusuario.TabIndex = 1;
             lblNombreusuario.Text = "Nombre de Usuario";
             // 
             // txtNombreusuario
             // 
-            txtNombreusuario.Location = new Point(14, 107);
-            txtNombreusuario.Margin = new Padding(3, 4, 3, 4);
+            txtNombreusuario.Location = new Point(12, 80);
             txtNombreusuario.Name = "txtNombreusuario";
-            txtNombreusuario.Size = new Size(245, 27);
+            txtNombreusuario.Size = new Size(215, 23);
             txtNombreusuario.TabIndex = 2;
             // 
             // txtContraseñanueva
             // 
             txtContraseñanueva.Enabled = false;
-            txtContraseñanueva.Location = new Point(14, 320);
-            txtContraseñanueva.Margin = new Padding(3, 4, 3, 4);
+            txtContraseñanueva.Location = new Point(12, 240);
             txtContraseñanueva.Name = "txtContraseñanueva";
             txtContraseñanueva.PasswordChar = '*';
-            txtContraseñanueva.Size = new Size(245, 27);
+            txtContraseñanueva.Size = new Size(215, 23);
             txtContraseñanueva.TabIndex = 4;
             // 
             // lblContraseña1
             // 
             lblContraseña1.AutoSize = true;
             lblContraseña1.ForeColor = Color.DarkBlue;
-            lblContraseña1.Location = new Point(14, 296);
+            lblContraseña1.Location = new Point(12, 222);
             lblContraseña1.Name = "lblContraseña1";
-            lblContraseña1.Size = new Size(127, 20);
+            lblContraseña1.Size = new Size(102, 15);
             lblContraseña1.TabIndex = 3;
             lblContraseña1.Text = "Nueva contraseña";
             // 
@@ -98,10 +96,9 @@
             // 
             btnEnviarcodigo.BackColor = Color.RoyalBlue;
             btnEnviarcodigo.ForeColor = Color.Black;
-            btnEnviarcodigo.Location = new Point(14, 145);
-            btnEnviarcodigo.Margin = new Padding(3, 4, 3, 4);
+            btnEnviarcodigo.Location = new Point(12, 109);
             btnEnviarcodigo.Name = "btnEnviarcodigo";
-            btnEnviarcodigo.Size = new Size(246, 37);
+            btnEnviarcodigo.Size = new Size(215, 28);
             btnEnviarcodigo.TabIndex = 5;
             btnEnviarcodigo.Text = "Enviar Codigo";
             btnEnviarcodigo.UseVisualStyleBackColor = false;
@@ -112,28 +109,27 @@
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.DarkBlue;
-            label3.Location = new Point(282, 173);
+            label3.Location = new Point(247, 130);
             label3.Name = "label3";
-            label3.Size = new Size(246, 28);
+            label3.Size = new Size(197, 21);
             label3.TabIndex = 7;
             label3.Text = "¿Olvidate tu contraseña?";
             // 
             // lblmensaje
             // 
             lblmensaje.AutoSize = true;
-            lblmensaje.Location = new Point(313, 201);
+            lblmensaje.Location = new Point(274, 151);
             lblmensaje.Name = "lblmensaje";
-            lblmensaje.Size = new Size(193, 100);
+            lblmensaje.Size = new Size(153, 75);
             lblmensaje.TabIndex = 8;
             lblmensaje.Text = "No te preocupes, podemos \r\nayudarte a reestablecerla. \r\nIngresa el usuario con el \r\ncual iniciaste sesion \r\nanteriormente.";
             // 
             // btnConfirmar
             // 
             btnConfirmar.Cursor = Cursors.Hand;
-            btnConfirmar.Location = new Point(14, 439);
-            btnConfirmar.Margin = new Padding(3, 4, 3, 4);
+            btnConfirmar.Location = new Point(12, 329);
             btnConfirmar.Name = "btnConfirmar";
-            btnConfirmar.Size = new Size(246, 35);
+            btnConfirmar.Size = new Size(215, 26);
             btnConfirmar.TabIndex = 10;
             btnConfirmar.Text = "Guardar";
             btnConfirmar.UseVisualStyleBackColor = true;
@@ -141,10 +137,9 @@
             // 
             // btnVolver
             // 
-            btnVolver.Location = new Point(282, 440);
-            btnVolver.Margin = new Padding(3, 4, 3, 4);
+            btnVolver.Location = new Point(247, 330);
             btnVolver.Name = "btnVolver";
-            btnVolver.Size = new Size(206, 35);
+            btnVolver.Size = new Size(180, 26);
             btnVolver.TabIndex = 11;
             btnVolver.Text = "Volver al Inicio de sesion";
             btnVolver.UseVisualStyleBackColor = true;
@@ -153,48 +148,46 @@
             // 
             lblConfirmarcontraseña.AutoSize = true;
             lblConfirmarcontraseña.ForeColor = Color.DarkBlue;
-            lblConfirmarcontraseña.Location = new Point(14, 355);
+            lblConfirmarcontraseña.Location = new Point(12, 266);
             lblConfirmarcontraseña.Name = "lblConfirmarcontraseña";
-            lblConfirmarcontraseña.Size = new Size(151, 20);
+            lblConfirmarcontraseña.Size = new Size(122, 15);
             lblConfirmarcontraseña.TabIndex = 12;
             lblConfirmarcontraseña.Text = "Confirmar contraseña";
             // 
             // txtConfirmarcontraseña
             // 
             txtConfirmarcontraseña.Enabled = false;
-            txtConfirmarcontraseña.Location = new Point(14, 379);
-            txtConfirmarcontraseña.Margin = new Padding(3, 4, 3, 4);
+            txtConfirmarcontraseña.Location = new Point(12, 284);
             txtConfirmarcontraseña.Name = "txtConfirmarcontraseña";
             txtConfirmarcontraseña.PasswordChar = '*';
-            txtConfirmarcontraseña.Size = new Size(245, 27);
+            txtConfirmarcontraseña.Size = new Size(215, 23);
             txtConfirmarcontraseña.TabIndex = 13;
             // 
             // lblCodigoenviado
             // 
             lblCodigoenviado.AutoSize = true;
             lblCodigoenviado.ForeColor = Color.DarkBlue;
-            lblCodigoenviado.Location = new Point(74, 215);
+            lblCodigoenviado.Location = new Point(65, 161);
             lblCodigoenviado.Name = "lblCodigoenviado";
-            lblCodigoenviado.Size = new Size(115, 20);
+            lblCodigoenviado.Size = new Size(91, 15);
             lblCodigoenviado.TabIndex = 14;
             lblCodigoenviado.Text = "Codigo Enviado";
             // 
             // txtCodigoenviado
             // 
             txtCodigoenviado.Enabled = false;
-            txtCodigoenviado.Location = new Point(14, 239);
-            txtCodigoenviado.Margin = new Padding(3, 4, 3, 4);
+            txtCodigoenviado.Location = new Point(12, 179);
             txtCodigoenviado.Name = "txtCodigoenviado";
             txtCodigoenviado.PlaceholderText = "Ingrese el codigo recibido";
-            txtCodigoenviado.Size = new Size(245, 27);
+            txtCodigoenviado.Size = new Size(215, 23);
             txtCodigoenviado.TabIndex = 15;
             txtCodigoenviado.TextAlign = HorizontalAlignment.Center;
             // 
             // frmRecuperarContrasena
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(521, 491);
+            ClientSize = new Size(456, 368);
             Controls.Add(txtCodigoenviado);
             Controls.Add(lblCodigoenviado);
             Controls.Add(txtConfirmarcontraseña);
@@ -209,10 +202,9 @@
             Controls.Add(txtNombreusuario);
             Controls.Add(lblNombreusuario);
             Controls.Add(lblNombreRecu);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "frmRecuperarContrasena";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "frmRecuperarContraseña";
+            Text = "FEXI - Recuperar Contraseña";
             Load += frmRecuperarContrasena_Load;
             ResumeLayout(false);
             PerformLayout();
